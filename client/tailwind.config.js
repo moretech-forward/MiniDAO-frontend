848bfa2b4d33deefc79baa46b30da5060d208b7f
Zxvum <zxvum.rect@gmail.com>
2024-07-21 17:30:41 +0300

    fix ui for forward

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,26 +1,52 @@
 const options = require("./config"); //options from config.js
 
-const allPlugins = {
-    typography: require("@tailwindcss/typography"),
-    // forms: require("@tailwindcss/forms"),
-    containerQueries: require("@tailwindcss/container-queries"),
-};
-
-const plugins = Object.keys(allPlugins)
-    .filter((k) => options.plugins[k])
-    .map((k) => {
-        if (k in options.plugins && options.plugins[k]) {
-            return allPlugins[k];
-        }
-    });
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    important: true,
-    content: ["./src/**/*.{html,js,php}"],
-    darkMode: "class",
-    theme: {
-        extend: {},
-    },
-    plugins: plugins,
+  important: true,
+  content: ["./src/**/*.{html,js,php}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
 };
